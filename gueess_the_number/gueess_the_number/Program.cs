33e946a535d58b1916f9166942142be695ee8785
1domin0 <dominik.grudzinski2004@gmail.com>
2021-10-02 22:12:39 +0200

    1.0

--- gueess_the_number/gueess_the_number/Program.cs
+++ gueess_the_number/gueess_the_number/Program.cs
@@ -6,7 +6,28 @@ namespace gueess_the_number
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            int x = rnd.Next(1, 10);
+            int y = 0;
+
+            Console.WriteLine(x);
+
+            while (y != x)
+            {
+                y = int.Parse(Console.ReadLine());
+                if (y < x)
+                {
+                    Console.WriteLine("Number X is bigger");
+                }
+                if (y > x)
+                {
+                    Console.WriteLine("Nu,ber X is smaller");
+                }
+                if (y == x)
+                {
+                    Console.WriteLine("You guessed the number!!");
+                }
+            }
         }
     }
 }
