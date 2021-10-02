299d9018f8495e0b4aaed3e794bfb6f15e2a1eba
1domin0 <dominik.grudzinski2004@gmail.com>
2021-10-02 22:08:54 +0200

    1.0

---
+++ gueess_the_number/gueess_the_number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gueess_the_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
