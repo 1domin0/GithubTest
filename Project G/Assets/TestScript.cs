6dc1f3044efdf25dce5bdc9f2a7b9b6b08f6844c
1domin0 <89093835+1domin0@users.noreply.github.com>
2021-10-03 01:32:55 +0200

    Update TestScript.cs

--- Project G/Assets/TestScript.cs
+++ Project G/Assets/TestScript.cs
@@ -9,9 +9,9 @@ public class TestScript : MonoBehaviour
         //Bubble sort
         int[] x = new int[] {1,9,4,8,3,7,2,5,6};
         int y;
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < x.Length - 1; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < x.Length - 1; j++)
             {
                 if (x[j] > x[j + 1])
                 {
