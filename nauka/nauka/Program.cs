01471c10df368fbdd39d4043175808b008ff3773
1domin0 <dominik.grudzinski2004@gmail.com>
2021-10-29 02:04:52 +0200

    1

--- nauka/nauka/Program.cs
+++ nauka/nauka/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Text;
 
 namespace nauka
 {
@@ -9,11 +13,13 @@ namespace nauka
         {
             int x = 10; int z = 20;
             Console.WriteLine($"x = {x} z = {z}");
-
+            
             Konstruktory();
             Klasy();
             Console.WriteLine(GenerowanieTekstu.WyswietlTekst("Mam na imie #0 i mam #1 lat", "Dominik", 17));
             Properties();
+            StringBuilderTest(1000000);
+            CreateNewCulture();
 
             Console.ReadKey();
         }
@@ -46,6 +52,57 @@ namespace nauka
             props.Prop = 11;
             Console.WriteLine("");
             Console.WriteLine($"Properties {props.Prop}");
+            Console.WriteLine("");
+        }
+        static void StringBuilderTest(int numberOfLoops)
+        {
+            string tekst1 = "tekst1: ";
+            string tekst2 = "tekst2: ";
+            int loops = numberOfLoops;
+
+            Stopwatch stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            for (int i = 0; i < numberOfLoops; i++)
+            {
+                tekst1 = tekst1 + i;
+            }
+            stopWatch.Stop();
+            Console.WriteLine("Tekst1: " + stopWatch.ElapsedMilliseconds + "ms");
+            stopWatch.Reset();
+
+            stopWatch.Start();
+            StringBuilder newStringBuilder = new StringBuilder();
+            for (int i = 0; i < numberOfLoops; i++)
+            {
+                newStringBuilder.Append(i);
+            }
+            stopWatch.Stop();
+            Console.WriteLine("Tekst2: " + stopWatch.ElapsedMilliseconds + "ms");
+            stopWatch.Reset();
+
+            Console.WriteLine("");
+        }
+        static void CreateNewCulture()
+        {
+            string str = "123";
+            float npint = float.Parse(str) + 2.23f;
+            var newCulture = (CultureInfo)CultureInfo.CurrentCulture.Clone();
+            newCulture.DateTimeFormat.ShortDatePattern = "D/m/yyyy";
+            newCulture.NumberFormat.NumberDecimalSeparator = "*";
+
+            Console.WriteLine(npint);
+            Console.WriteLine(CultureInfo.CurrentCulture.Name);
+            Console.WriteLine(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern);
+
+            CultureInfo.CurrentCulture = newCulture;
+ 
+            Console.WriteLine(npint);
+            Console.WriteLine(CultureInfo.CurrentCulture.Name);
+            Console.WriteLine(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern);
+
+            CultureInfo.CurrentCulture = new CultureInfo("en-US", false);
+
             Console.WriteLine("");
         }
 
